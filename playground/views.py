8262db6f29d44dbee73eb51a619b762f5a46f1da
Your Name <shashank.dhavala@gmail.com>
2023-04-17 00:56:01 +0530

    Functionality of login, signup is implemented

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 # More of a request handler than a front end "view" perspective.
+# View function --> Takes a request and returns a response
+# Also called an action in other frameworks
+# URLs need to be mapped to views
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def signup_user(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
