0b1bc7e9de57cfbe57300b190405025861bd6f9b
ahindra2002 <ah.prayaga@gmail.com>
2023-04-27 12:00:28 +0530

    added models and forms

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+class Doctor(models.Model):
+    email_id=models.EmailField(help_text="We take mail-id for username",max_length=20)
+    first_name = models.CharField(max_length = 200)
+    last_name = models.CharField(max_length = 200)
+    nmc_id = models.IntegerField(help_text = "Enter your IMR registration number")
+    state=models.CharField(help_text="Which state's medical council?",max_length=15)
+    year=models.CharField(max_length=5)
+    
--- playground/urls.py
+++ playground/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
     path('',views.home,name='home'),
     path('login/', views.login_user, name='login'),
-    path('signup/',views.signup_user, name='signup')
+    path('signup/',views.signup_user, name='signup'),
+    path('signup/verify_doctor',views.verify_doctor,name='verify')
 ]
